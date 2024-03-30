edae02d9ec312ad43fbe9204380c287cfcfcee06
Sahel Sharifymoghaddam <sahel.sharifi@gmail.com>
2024-03-30 13:37:30 -0400

    update bash file (#9)
    This cl makes the following changes to the bash file:
    
    get the image count from ls
    increase the batch size to 512
    minor clean up

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 export PYSERINI_CACHE=/store2/scratch/s8sharif
 
-#!/bin/bash
-bs=96
-# for ((i = 0; i < 10126; i += $bs)); do
-#     next_index=$((i + bs))
-#     CUDA_VISIBLE_DEVICES=0 python inference.py --prompt_file caption-prompt-with-examples.txt \
-#     --k=10 --model_name llava \
-#     --output_dir /store2/scratch/s8sharif/UniIR/llm_outputs \
-#     --retrieved_results_path "/store2/scratch/s8sharif/UniIR/data/UniIR/retrieval_results/CLIP_SF/Large/Instruct/InBatch/run_files/mbeir_mscoco_task3_union_pool_test_k10_run_2024-03-27 15:28:49.276449.jsonl" \ 
-#     --index $i"_"$next_index --retriever_name "CLIP_SF"
-# done
-
 # TODO: make the bash file work with flag values for different runs
+bs=512
+image_path=/mnt/users/s8sharif/M-BEIR/mbeir_images/mscoco_images/val2014
+image_count=$(ls $image_path | wc -l)
+echo $image_count
+k=5
 
-for ((i = 0; i < 10126; i += $bs)); do
+for ((i = 0; i < $image_count; i += $bs)); do
     next_index=$((i + bs))
-    CUDA_VISIBLE_DEVICES=7 python inference.py --prompt_file caption-prompt-without-examples.txt \
-    --k=0 --model_name llava \
-    --image_path /mnt/users/s8sharif/M-BEIR/mbeir_images/mscoco_images/val2014 \
+    CUDA_VISIBLE_DEVICES=7 python inference.py --prompt_file caption-prompt-with-examples.txt \
+    --k=$k --model_name llava \
+    --image_path $image_path \
     --output_dir /store2/scratch/s8sharif/UniIR/llm_outputs \
     --retrieved_results_path "/store2/scratch/s8sharif/UniIR/data/UniIR/retrieval_results/CLIP_SF/Large/Instruct/InBatch/run_files/mbeir_mscoco_task3_union_pool_test_k10_run_2024-03-27 15:28:49.276449.jsonl" \
     --index $i"_"$next_index --retriever_name "CLIP_SF"
 done
+
+
+
+# for ((i = 0; i < $image_count; i += $bs)); do
+#     next_index=$((i + bs))
+#     CUDA_VISIBLE_DEVICES=7 python inference.py --prompt_file caption-prompt-without-examples.txt \
+#     --k=$k --model_name llava \
+#     --image_path $image_path \
+#     --output_dir /store2/scratch/s8sharif/UniIR/llm_outputs \
+#     --retrieved_results_path "/store2/scratch/s8sharif/UniIR/data/UniIR/retrieval_results/CLIP_SF/Large/Instruct/InBatch/run_files/mbeir_mscoco_task3_union_pool_test_k10_run_2024-03-27 15:28:49.276449.jsonl" \
+#     --index $i"_"$next_index --retriever_name "CLIP_SF"
+# done
