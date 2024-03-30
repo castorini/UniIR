bced219174838916d183aa2bc5d87902e89cf752
sahel <sahel.sharifi@gmail.com>
2024-03-30 18:39:54 -0400

    remove unwanted import

--- scripts/eval.py
+++ scripts/eval.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 import jsonlines
 from pycocoevalcap.bleu.bleu import Bleu
-from pycocoevalcap.cider.cider import Cider, CiderScorer
+from pycocoevalcap.cider.cider import Cider
 from pycocoevalcap.meteor.meteor import Meteor
 from pycocoevalcap.rouge.rouge import Rouge
 from pycocoevalcap.spice.spice import Spice
